114505e96b92bdd1551068a6a65abcdd397be405
Jamie Mellway <jamie.mellway@gmail.com>
2023-07-26 20:38:37 -0400

    Turn of Geolocation

--- streamlit/home.py
+++ streamlit/home.py
@@ -7,17 +7,19 @@ import montecarlo
 import dataload
 import machinelearning
 import chatbot
+import unittest
 
 def main():
     app = MultiApp()
 
     app.add_app("Trends (Ontario)", priceindextrends.render_page)
     #app.add_app("Trends (Regional)", priceindextrendsregional.render_page)
-    #app.add_app("Geolocation", geolocation.render_page)
+    app.add_app("Geolocation", geolocation.render_page)
     app.add_app("Monte Carlo", montecarlo.render_page)
     app.add_app("Machine Learning - Data Load", dataload.render_page)
     app.add_app("Machine Learning", machinelearning.render_page)
     app.add_app("Property Value", chatbot.render_page)
+    app.add_app("Unit Tests", unittest.render_page)
     
     # The main app
     app.run()
