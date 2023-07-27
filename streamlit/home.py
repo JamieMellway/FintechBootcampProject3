494b19899096abca256f7068cd7aafd4f3a7cf05
Jamie Mellway <jamie.mellway@gmail.com>
2023-07-26 21:18:15 -0400

    Removed Geolocation

--- streamlit/home.py
+++ streamlit/home.py
@@ -14,12 +14,12 @@ def main():
 
     app.add_app("Trends (Ontario)", priceindextrends.render_page)
     #app.add_app("Trends (Regional)", priceindextrendsregional.render_page)
-    app.add_app("Geolocation", geolocation.render_page)
+    #app.add_app("Geolocation", geolocation.render_page)
     app.add_app("Monte Carlo", montecarlo.render_page)
     app.add_app("Machine Learning - Data Load", dataload.render_page)
     app.add_app("Machine Learning", machinelearning.render_page)
     app.add_app("Property Value", chatbot.render_page)
-    #app.add_app("Unit Tests", unittest.render_page)
+    app.add_app("Unit Tests", unittest.render_page)
     
     # The main app
     app.run()
